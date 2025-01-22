9bc773724c9cc0b156288e8b2c760db6afd90e61
JDM_David <fer.mtz.guz@gmail.com>
2025-01-22 11:05:06 -0600

    Add initial project files and npmignore configurations

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./frontend/**/*.{html,js}", 
+    "./backend/CSS/**/*.css",
+    "./backend/templates/**/*.html",
+    "./frontent/**/*.html",
+    "./frontend/pages/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
